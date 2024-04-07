511d40c702a204aeaece8fe40476db07c10c647c
Gulfani Putra <gulfaniputra@gmail.com>
2024-04-07 21:43:54 +0800

    Wrap multiple 'Square' components with a <div>

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,17 @@ function Square() {
 
 function App() {
   return (
-    <>
+    <div className="board">
       <Square />
-    </>
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+      <Square />
+    </div>
   );
 }
 
